0137f98c9bbb3314b648ac7ddd3b94d1a58f74ca
blackyselva <selvam17196@gmail.com>
2023-03-19 17:11:10 +0530

    Adding.

---
+++ main.js
@@ -0,0 +1,13 @@
+document.write("4. Enter N odd number loop");
+
+document.write("<br>");
+document.write("<br>");
+
+var n=prompt("Enter a odd N number");
+for (i=0; i<=n; ++i) {
+	if (i++) {
+	
+	}
+	document.write(i);
+	document.write("<br>");
+}
